a37bd2dde04f849f5abb0cce498d6f6618871a02
adryanhy <adryanhy1@gmail.com>
2025-02-05 15:54:40 -0300

    Configuração correta do tailwind css na página

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}', // Certifique-se de que está apontando para a pasta src/app
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
